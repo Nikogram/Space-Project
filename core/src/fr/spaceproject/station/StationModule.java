544fd659757e5e5801d723d2d3f267e5007f03a5
Nikogram <nikogram@users.noreply.github.com>
2016-01-19 15:23:41 +0100

    Recuperation

--- core/src/fr/spaceproject/station/StationModule.java
+++ core/src/fr/spaceproject/station/StationModule.java
@@ -1,5 +1,6 @@
 package fr.spaceproject.station;
 
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 import fr.spaceproject.utils.Orientation;
@@ -9,18 +10,59 @@ import fr.spaceproject.utils.Vec2f;
 
 public class StationModule
 {
+	private TextureManager textureManager;
 	private Sprite sprite;
 	private int type;
+	private int level;
+	private float energy;
 	
 	public StationModule(int type, int level, Vec2f position, Orientation orientation, TextureManager textureManager)
 	{
+		this.textureManager = textureManager;
+		sprite = new Sprite(position, new Vec2f(140, 140), getTexture());
 		this.type = type;
-		sprite = new Sprite(position, new Vec2f(140, 140), textureManager.getTexture("SimpleStationModule"));
+		this.level = level;
+		energy = getMaxEnergy();
+	}
+	
+	public Texture getTexture()
+	{
+		if (type == 1)	// Simple
+			return textureManager.getTexture("SimpleStationModule");
+		else	// Simple
+			return textureManager.getTexture("SimpleStationModule");
+	}
+	
+	public float getMaxEnergy()
+	{
+		return 5000 + 1000 * (level - 1);
 	}
 	
 	public int getType()
 	{
 		return type;
+	}
+	
+	public int getLevel()
+	{
+		return type;
+	}
+	
+	public float getEnergy()
+	{
+		return energy;
+	}
+	
+	public Sprite getSprite()
+	{
+		return sprite.clone();
+	}
+	
+	public Sprite getSprite(boolean copy)
+	{
+		if (copy)
+			return sprite.clone();
+		return sprite;
 	}
 	
 	public void draw(SpriteBatch display)
