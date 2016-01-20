e1d458918b7ba64d2311d462369bd5c64ff23e8b
hahio <deniauxr@hotmail.fr>
2016-01-20 16:46:03 +0100

    statevessel

--- core/src/fr/spaceproject/game/Game.java
+++ core/src/fr/spaceproject/game/Game.java
@@ -86,7 +86,7 @@ public class Game extends ApplicationAdapter
 		map = new WarMap();
 		state = new Geopolitics(5);
 		
-		zone = new SectorMap(1500,new Coor(0,0),0, textureManager,state,map);
+		zone = new SectorMap(1500,new Coor(0,0),22, textureManager,state,map);
 		
 		carte =new FactionMap(zone.getVector().get(0).getPosition(),zone.getCoor(),map, textureManager);
 		stateVessel=new VesselState(zone.getVector().get(0),textureManager);
